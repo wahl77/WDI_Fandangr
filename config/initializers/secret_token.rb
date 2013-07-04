753cab11d05b1e3c7768898e00ee6ecd9f92dd36
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-07-04 16:10:43 -0700

    Blank, ready to work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fandangr::Application.config.secret_token = 'ef3ae46a41fc6e6ebe505edc91f739d7d5aa52f2deca7cc6df33239b6e54d1b5d793bfd3f227fdfbc6c5c7b546f4aaefb586a815bd3b69517b552f665344603d'
